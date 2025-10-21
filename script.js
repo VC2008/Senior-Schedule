352a34ed3dad0d09a63b3ee83240986a05bae8b9
VC2008 <526Vcolon@frhsd.com>
2025-10-21 13:19:47 -0400

    CORRECTLY naming the json file

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 function async (params) {
      const xhr = new XMLHttpRequest();
-  xhr.open("GET", "list.json", true);
+  xhr.open("GET", "Vincent.json", true);
   xhr.onload = function() {
     if (xhr.status === 200) {
       const data = JSON.parse(xhr.responseText);
       data.forEach({className, teacher, roomNumber, period, subjectArea, }) => {
+        const div = `<div class" col-12 col-sm-6 col-lg-4"> `
 }
 }
   {
