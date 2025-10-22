56c1e4a396f01d8f62bfb3e79be23ca17f0a90fb
VC2008 <526Vcolon@frhsd.com>
2025-10-22 17:22:36 +0000

    Trying something else to fix this

--- script.js
+++ script.js
@@ -2,30 +2,31 @@ const scheduleContainer = document.getElementById('scheduleContainer');
 const status = document.getElementById('status');
 const selector = document.getElementById('friendSelector');
 
-function renderSchedule(fileName) {
-  const data = schedules[fileName];
-  if (!data) {
+function renderSchedule(jsonfolder) {
+  const data = schedules[jsonfolder];
+  if (!jsonfolder) {
      status.innerHTML = `<div class="alert alert-danger">Schedule not found!</div>`;
     scheduleContainer.innerHTML = "";
     return;
   }
 
 
-  status.innerHTML = "";
+  status.innerHTML = "jsonfolder";
   scheduleContainer.innerHTML = "";
 
 
 
   data.sort((a,b)=> a.period - b.period);
 
-  data.forEach(cls => {
+  data.forEach(jsonfolder => 
+{
     const html = `
       <div class="card col-md-3 p-3 mb-3 opacity-1">
-        <h5>${cls.className}</h5>
-        <p><strong>Teacher:</strong> ${Array.isArray(cls.teacher) ? cls.teacher.join(', ') : cls.teacher}</p>
-        <p><strong>Room:</strong> ${cls.roomNumber}</p>
-        <p><strong>Period:</strong> ${cls.period}</p>
-        <p><strong>Subject:</strong> ${cls.subjectArea}</p>
+        <h5>${jsonfolder.className}</h5>
+        <p><strong>Teacher:</strong> ${Array.isArray(jsonfolder.teacher) ? jsonfolder.teacher.join(', ') : jsonfolder.teacher}</p>
+        <p><strong>Room:</strong> ${jsonfolder.roomNumber}</p>
+        <p><strong>Period:</strong> ${jsonfolder.period}</p>
+        <p><strong>Subject:</strong> ${jsonfolder.subjectArea}</p>
       </div>
     `;
     scheduleContainer.insertAdjacentHTML('beforeend', html);
